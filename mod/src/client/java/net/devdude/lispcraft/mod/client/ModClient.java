e4c93470f9d25e2035d224c5a53c1f92cdbcf965
Edison Moreland <edison@devdude.net>
2024-08-15 18:09:10 -0700

    Console Block

--- mod/src/client/java/net/devdude/lispcraft/mod/client/ModClient.java
+++ mod/src/client/java/net/devdude/lispcraft/mod/client/ModClient.java
@@ -2,23 +2,12 @@ package net.devdude.lispcraft.mod.client;
 
 import net.devdude.lispcraft.mod.Mod;
 import net.devdude.lispcraft.mod.client.screens.ConsoleScreen;
-import net.devdude.lispcraft.runtime.EvalTest;
-import net.devdude.lispcraft.runtime.Printer;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 public class ModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EvalTest.hello(new ModPrinter());
         HandledScreens.register(Mod.ScreenHandlers.CONSOLE, ConsoleScreen::new);
-    }
-
-    private static class ModPrinter implements Printer {
-
-        @Override
-        public void print(String string) {
-            System.out.println(string);
-        }
     }
 }
--- runtime/src/main/java/net/devdude/lispcraft/runtime/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package net.devdude.lispcraft.runtime;
-
-public class HelloWorld {
-    public static String HELLO = "Hello World!";
-}
